f8518094e2391c53de2bce3d18acd17c100dc86c
Li, Ni <nli306@gatech.edu>
2021-11-22 22:33:04 -0600

    add instructions and dotenv config (#3)
    Co-authored-by: Ni Li <yuyusky38@gmail.com>

--- phase3_project_in_progress/app/OLDapp.py
+++ phase3_project_in_progress/app/OLDapp.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
-
+import os
 
 app = Flask(__name__)
 
-
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'CS6400'
-app.config['MYSQL_DB'] = 'CS6400_try1'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_PORT'] = 3307
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'CS6400')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'CS6400_try1')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_PORT'] = os.getenv('MYSQL_PORT', 3307)
 
 mysql = MySQL(app)
 
--- phase3_project_in_progress/app/__init__.py
+++ phase3_project_in_progress/app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'CS6400'
-app.config['MYSQL_DB'] = 'CS6400_try1'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_PORT'] = 3307
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'CS6400')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'CS6400_try1')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_PORT'] = os.getenv('MYSQL_PORT', 3307)
 app.config['SECRET_KEY'] = 'my_keys'
-
 
 from app import report_part_stats, report_average_time, report_sales_by_color, report_monthly_sales, \
                 report_repair_by_manu, report_customer_income, login, search, add_vehicle, \
