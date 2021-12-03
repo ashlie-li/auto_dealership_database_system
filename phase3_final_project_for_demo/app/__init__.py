67c2e268937bf0d15e4ccc6c09002aa86940eb50
Yanlin Wu <yanlinwu2009@gmail.com>
2021-12-03 16:18:25 -0500

    12-03 submission2

--- phase3_final_project_for_demo/app/__init__.py
+++ phase3_final_project_for_demo/app/__init__.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = 'my_keys'
 from app import report_part_stats, report_average_time, report_sales_by_color, report_monthly_sales, \
                 report_below_cost_sales, report_sales_by_type, report_sales_by_manufacturer, \
                 report_repair_by_manu, report_customer_income, login, search, add_vehicle, vehicle_detail,\
-                search_add_customer,add_sales, main_menu, testing, logout, add_repair2, test_export
+                search_add_customer,add_sales, main_menu, logout, add_repair2
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5000)
